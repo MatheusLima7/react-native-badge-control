798d52cbb8e11f9597c05b99a7c64c1c05080561
Matheus Lima <matheuslima@MacBook-Pro-de-Matheus.local>
2022-05-16 09:12:11 -0300

    chore: initial commit

---
+++ android/src/main/java/com/reactnativebadgecontrol/BadgeControlModule.java
@@ -0,0 +1,34 @@
+package com.reactnativebadgecontrol;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = BadgeControlModule.NAME)
+public class BadgeControlModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "BadgeControl";
+
+    public BadgeControlModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
